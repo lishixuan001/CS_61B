b235af313e9560051dc23cbab6708b961927d524
Wayne Li <lishixuan001@berkeley.edu>
2017-08-24 16:08:50 -0700

    Add my LeapYear

---
+++ lab1/LeapYear.java
@@ -0,0 +1,17 @@
+// Adapted from Sierra and Bates, Head First Java
+
+// You need not use all of the lines below, and you may use duplicates.
+public class LeapYear{
+	public static void main(String[] args) {
+		int year = 2000;
+		if ((year % 4 == 0) && (year % 100 != 0)) {
+			System.out.println (year + " is a leap year.");
+		}
+		else if (year % 400 == 0){
+			System.out.println (year + " is a leap year.");
+		}
+		else{
+			System.out.println (year + " is not a leap year.");
+		}
+	}
+}
