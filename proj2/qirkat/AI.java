d348cad23b4363ffab5857a66f6cdb588d3637a2
Shixuan Li <lishixuan001@berkeley.edu>
2017-11-11 23:46:16 -0800

    test proj2

--- proj2/qirkat/AI.java
+++ proj2/qirkat/AI.java
@@ -57,12 +57,7 @@ class AI extends Player {
      *  is a move. */
     private Move findMove() {
         Board b = new Board(board());
-        if (myColor() == WHITE) {
-            findMax(b, MAX_DEPTH, -INFTY, INFTY);
-        } else {
-            findMin(b, MAX_DEPTH, -INFTY, INFTY);
-        }
-        return _lastFoundMove;
+        return findMove(b, MAX_DEPTH, -INFTY, INFTY);
     }
 
     /** The move found by the last call to one of the ...FindMove methods
@@ -75,221 +70,30 @@ class AI extends Player {
      *  and minimal value or value < ALPHA if SENSE==-1. Searches up to
      *  DEPTH levels.  Searching at level 0 simply returns a static estimate
      *  of the board value and does not set _lastMoveFound. */
-    private int findMax(Board board, int depth, int alpha, int beta) {
-        Move best = null;
-        int bestScore = -INFTY;
-
+    private Move findMove(Board board, int depth, int alpha, int beta) {
         board.checkGameOver();
-        PieceColor winner = board.winner();
-
-        if (depth == 0 || winner.isPiece()) {
-            Board next = new Board(board);
-            return flatMax(next, alpha, beta);
-        }
-
         ArrayList<Move> moves = board.getMoves();
         PieceColor currentPlayer = board.whoseMove();
         ArrayList<Move> myMoves = board.getMyMoves(moves, currentPlayer);
-
-        myMoves = filter(myMoves);
-
-        for (Move mov : myMoves) {
-            Board next = new Board(board);
-            next.makeMove(mov);
-            int response = findMin(next, depth - 1, alpha, beta);
-
-            next.undo();
-            if (response >= bestScore) {
-                best = mov;
-                bestScore = response;
-                alpha = Math.max(alpha, bestScore);
-                if (alpha >= beta) {
-                    break;
-                }
-            }
-        }
-
-        _lastFoundMove = best;
-        return bestScore;
-    }
-
-    /** Find the minimum.
-     * @param alpha --input
-     * @param beta --input
-     * @param board --input
-     * @param depth --input
-     * @return */
-    private int findMin(Board board, int depth, int alpha, int beta) {
-        Move best = null;
-        int bestScore = INFTY;
-
-        board.checkGameOver();
-        PieceColor winner = board.winner();
-
-        if (depth == 0 || winner.isPiece()) {
-            Board next = new Board(board);
-            return flatMin(next, alpha, beta);
-        }
-
-        ArrayList<Move> moves = board.getMoves();
-        PieceColor currentPlayer = board.whoseMove();
-        ArrayList<Move> myMoves = board.getMyMoves(moves, currentPlayer);
-
-        myMoves = filter(myMoves);
-
-        for (Move mov : myMoves) {
-            Board next = new Board(board);
-            next.makeMove(mov);
-            int response = findMax(next, depth - 1, alpha, beta);
-            next.undo();
-            if (response <= bestScore) {
-                best = mov;
-                bestScore = response;
-                beta = Math.min(alpha, bestScore);
-                if (alpha >= beta) {
-                    break;
-                }
-            }
-        }
-
-        _lastFoundMove = best;
-        return bestScore;
-    }
-
-
-    /** Last condition for finding maximized possible move/jump.
-     * @param board --input
-     * @param beta --input
-     * @param alpha --input
-     * @return */
-    private int flatMax(Board board, int alpha, int beta) {
-        board.checkGameOver();
-        PieceColor winner = board.winner();
-        Move best = null;
-        int bestScore = -INFTY;
-
-        if (winner.isPiece()) {
-            if (winner.equals(WHITE)) {
-                bestScore = INFTY;
-            } else if (winner.equals(BLACK)) {
-                bestScore = -INFTY;
-            }
-            return bestScore;
-        }
-        ArrayList<Move> moves = board.getMoves();
-        PieceColor currentPlayer = board.whoseMove();
-        ArrayList<Move> myMoves = board.getMyMoves(moves, currentPlayer);
-
-        myMoves = filter(myMoves);
-
-        for (Move mv : myMoves) {
-            Board bd = new Board(board);
-            bd.makeMove(mv);
-            int score = staticScore(bd);
-            bd.undo();
-            if (score >= bestScore) {
-                best = mv;
-                bestScore = score;
-                alpha = Math.max(alpha, bestScore);
-                if (beta <= alpha) {
-                    break;
-                }
-            }
-        }
-        _lastFoundMove = best;
-        return bestScore;
-    }
-
-    /** Last condition for finding maximized possible move/jump.
-     * @param alpha --input
-     * @param beta --input
-     * @param board --input
-     * @return */
-    private int flatMin(Board board, int alpha, int beta) {
-
-        board.checkGameOver();
-        PieceColor winner = board.winner();
-        Move best = null;
-        int bestScore = INFTY;
-
-        if (winner.isPiece()) {
-            if (winner.equals(WHITE)) {
-                bestScore = INFTY;
-            } else if (winner.equals(BLACK)) {
-                bestScore = -INFTY;
-            }
-            return bestScore;
-        }
-
-        ArrayList<Move> moves = board.getMoves();
-        PieceColor currentPlayer = board.whoseMove();
-        ArrayList<Move> myMoves = board.getMyMoves(moves, currentPlayer);
-
-        myMoves = filter(myMoves);
-
-        for (Move mv : myMoves) {
-            Board bd = new Board(board);
-            bd.makeMove(mv);
-            int score = staticScore(bd);
-            bd.undo();
-            if (score <= bestScore) {
-                best = mv;
-                bestScore = score;
-                beta = Math.min(alpha, bestScore);
-                if (beta <= alpha) {
-                    break;
-                }
-            }
-        }
-        _lastFoundMove = best;
-        return bestScore;
-    }
-
-    /** Return a heuristic value for BOARD.*/
-    private int staticScore(Board board) {
-        String string = board.board();
-        int black = 0;
-        int white = 0;
-        for (int i = 0; i < string.length(); i++) {
-            char piece = string.charAt(i);
-            if (piece == 'b') {
-                black += 1;
-            } else if (piece == 'w') {
-                white += 1;
-            }
-        }
-        return white - black;
+        Move mov = filter(myMoves);
+        return mov;
     }
 
     /** Filter the moves.
      * @param moves --input
      * @return */
-    private ArrayList<Move> filter(ArrayList<Move> moves) {
-        ArrayList<Move> result = new ArrayList<>();
+    private Move filter(ArrayList<Move> moves) {
         Move first = null;
-        Move second = null;
         for (Move mov : moves) {
             int s = mov.length();
             if (first == null) {
                 first = mov;
-            } else if (second == null) {
-                second = mov;
             } else {
                 if (s >= first.length()) {
-                    Move temp = first;
                     first = mov;
-                    second = temp;
-                } else if (s >= second.length()) {
-                    second = mov;
                 }
             }
         }
-        if (first != null) {
-            result.add(first);
-        }
-        if (second != null) {
-            result.add(second);
-        }
-        return result;
+        return first;
     }
 }
